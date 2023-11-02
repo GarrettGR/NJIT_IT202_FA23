49709f871946cc616db6aaaf9dc3cf211def7011
Garrett Gonzalez-Rivas <emailforggr@gmail.com>
2023-11-02 14:47:20 +0000

    starting an in-class exersice (chapter 5)

---
+++ chapter 5/In-Class exersice/test_scores.js
@@ -0,0 +1,15 @@
+const scores = [];
+
+let score = 0;
+do {
+    // TODO ask user for score and assign to score
+    //if-else to validate input
+        //if score is valid (using isNAN function, and within 0 & 100) add score to array
+        //otherwise, alert error message
+} while(score != 1)
+
+if(scores.length > 0){
+    //display all scores using a for loop and document.write
+    //sum (and display) all values in the array
+    //calculate (and display) average of all values in array
+}
