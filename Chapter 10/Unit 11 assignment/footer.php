d6f6d74e8c589d831c931d7538b64d0ca1f67739
Garrett Gonzalez-Rivas <emailforggr@gmail.com>
2023-11-18 23:34:04 -0500

    deleting old webstie code -- starting over

--- Chapter 10/Unit 11 assignment/footer.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <footer>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12091.640837505438!2d-74.19203265527406!3d40.74200124412969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2534cc006098b%3A0xfac623bce8f114d8!2sMaple%20Hall%20NJIT!5e0!3m2!1sen!2sus!4v1696634464882!5m2!1sen!2sus" width="240" height="180" style="border:0; display:flex; justify-content:left" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        <p>Your pet's happiness and well-being are at the heart of all we do.</p>
-        <p style="display: inline">Copyright © Garrett Gonzalez-Rivas 2023</p>
-        <a href="mailto:grg@njit.edu">Contact Us!</a>
-    </footer>
-</html>
