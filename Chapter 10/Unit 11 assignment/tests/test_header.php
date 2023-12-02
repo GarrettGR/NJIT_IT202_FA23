8030c9c2eaeb24bd01843d55e69974d9d4710ff9
Garrett Gonzalez-Rivas <emailforggr@gmail.com>
2023-12-01 21:54:14 -0500

    replacing (fixing) the header

--- Chapter 10/Unit 11 assignment/tests/test_header.php
+++ Chapter 10/Unit 11 assignment/tests/test_header.php
@@ -1,110 +1,110 @@
-<header>
-  <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-    <!-- image and brand name -->
-    <a class="navbar-brand" href="index.php">
-      <img src="images\pet-store-icon.png" width="30" height="30" alt="" />
-      Hartrum's Pet Shop
-    </a>
+<html>
 
-    <!-- navbar toggler for responsive (small screens) -->
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavDropdown"
-      aria-controls="navbarNavDropdown"
-      aria-expanded="true"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- navbar menu buttons -->
-    <div class="navbar-collapse collapse show" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <!-- dropdown for the "home" page with section headings for auto-scroll -->
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Home
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="index.php">Home</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#goods">Goods</a>
-            <a class="dropdown-item" href="#animals">Animals</a>
-            <a class="dropdown-item" href="#services">Services</a>
-            <a class="dropdown-item" href="#description">Description</a>
-          </div>
-        </li>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="test0.css" />
 
-        <!-- other button options -->
-        <li class="nav-item">
-          <a class="nav-link" href="pets.php">Pets</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="rescue.php">Rescue</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="shipping.php">Shipping</a>
-        </li>
-      </ul>
-    </div>
+  <!-- jQuery JS -->
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
-    <!-- div holding login/logout button & modal -->
-    <div class="pull-left">
-      <!-- login button -->
-      <button
-        type="button"
-        class="btn btn-outline-secondary"
-        id="login-btn"
-        data-bs-toggle="modal"
-        data-bs-target="#login-modal"
-      >
-        Login
-      </button>
+  <!-- Popper.js and Boostrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 
-      <!-- login modal -->
-      <div
-        class="modal fade"
-        id="login-modal"
-        tabindex="-1"
-        aria-labelledby="login-modal"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal">
-                &times;
-              </button>
-              <h4 class="modal-title">Login</h4>
-            </div>
-            <div class="modal-body">
+
+  <!-- Custom JavaScript -->
+  <script src="../src/js/script.js"></script>
+</head>
+
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <!-- image and brand name -->
+        <a class="navbar-brand" href="index.php">
+          <img src="images\pet-store-icon.png" width="30" height="30" alt="" />
+          Hartrum's Pet Shop
+        </a>
+
+        <!-- navbar toggler for responsive (small screens) -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- navbar menu buttons -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <!-- dropdown for the "home" page with section headings for auto-scroll -->
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Home
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="index.php">Home</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="index.php#goods">Goods</a></li>
+                <li><a class="dropdown-item" href="index.php#Animals">Animals</a></li>
+                <li><a class="dropdown-item" href="index.php#services">Services</a></li>
+                <li><a class="dropdown-item" href="index.php#description">Description</a></li>
+              </ul>
+            </li>
+
+            <!-- other button options -->
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="pets.php">Pets</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="rescue.php">Rescue</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">Shipping</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- div holding login/logout button & modal -->
+      <div class="login pull-left">
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#ModalForm">
+          Login
+        </button>
+      </div>
+    </nav>
+
+    <div class="modal fade" tabindex="-1" role="dialog" id="ModalForm" aria-labelledby="ModalFormLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-body">
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="myform bg-dark">
+              <h1 class="text-center">Login Form</h1>
               <form>
-                <h1>LOGIN FORM HERE</h1>
+                <div class="mb-3 mt-4">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" />
+                </div>
+                <button type="submit" class="btn btn-light mt-3">LOGIN</button>
+                <p>Not a member? <a href="#">Signup now</a></p>
               </form>
-            </div>
-            <div class="modal-footer">
-              <button
-                type="submit"
-                class="btn btn-danger btn-default pull-left"
-                data-dismiss="modal"
-              >
-                <span class="glyphicon glyphicon-remove"></span>
-                Cancel
-              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </nav>
-</header>
+
+  </header>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
